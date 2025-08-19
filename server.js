2ee84581215150bf5dca8ca4b2a7cffd21ed15d0
Rashmitha S P <rashmithasp85@gmail.com>
2025-08-19 21:26:58 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  let filePath = path.join(
+    __dirname,
+    "public",
+    req.url === "/" ? "index.html" : req.url
+  );
+
+  let extname = path.extname(filePath);
+  let contentType = "text/html";
+
+  switch (extname) {
+    case ".css":
+      contentType = "text/css";
+      break;
+    case ".js":
+      contentType = "text/javascript";
+      break;
+    case ".json":
+      contentType = "application/json";
+      break;
+    case ".png":
+      contentType = "image/png";
+      break;
+    case ".jpg":
+      contentType = "image/jpg";
+      break;
+  }
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.writeHead(404, { "Content-Type": "text/html" });
+      res.end("<h1>404 Not Found</h1>", "utf-8");
+    } else {
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(content, "utf-8");
+    }
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`🚀 Resume website running at http://localhost:${PORT}`);
+});
